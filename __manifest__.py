574e7c0f632316f05b2cbff29c4db1ca927995da
Mario Roberto <gomez@thetecwave.com>
2025-11-27 16:51:27 -0600

    Añadir campo de mensaje personalizado y pestaña de personalización a los pedidos de venta
    - Añadir el campo mr_personalised_message al modelo sale.order
    - Añadir la pestaña Personalización en el formulario de pedido de venta
    - Actualizar la plantilla del informe para utilizar el mensaje personalizado de forma dinámica

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,17 @@
 {
-    "name": "MRG Sale Custom Quotation Template",
+    "name": "Plantilla de Presupuesto Personalizado de Venta",
     "summary": "Custom PDF quotation template for sale orders.",
     "version": "16.0.1.0.0",
     "category": "Sales/Reporting",
-    "author": "MRG",
-    "website": "https://mrgomez.example.com",
+    "author": "Mario Roberto",
+    "website": "https://mrgomezsv.github.io/",
     "license": "LGPL-3",
     "depends": [
         "sale", "web",
     ],
     "data": [
         "data/paper_format.xml",
+        "views/sale_order_views.xml",
         "report/sale_order_report_templates.xml",
     ],
     "installable": True,
